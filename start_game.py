f480e9594218d7f6cc3d0f8ad803142695e573d0
GotinhaAzul <d4v1santiago@gmail.com>
2026-02-08 19:23:08 -0300

    Instala pygame e colorama automaticamente ao iniciar

--- start_game.py
+++ start_game.py
@@ -1,3 +1,33 @@
+import importlib.util
+import subprocess
+import sys
+
+
+DEPENDENCIAS_OBRIGATORIAS = {
+    "pygame": "pygame",
+    "colorama": "colorama",
+}
+
+
+def _instalar_dependencias_ausentes() -> None:
+    ausentes = [
+        pacote
+        for modulo, pacote in DEPENDENCIAS_OBRIGATORIAS.items()
+        if importlib.util.find_spec(modulo) is None
+    ]
+
+    if not ausentes:
+        return
+
+    print(
+        "Dependências ausentes detectadas. Instalando automaticamente: "
+        + ", ".join(ausentes)
+    )
+    subprocess.check_call([sys.executable, "-m", "pip", "install", *ausentes])
+
+
+_instalar_dependencias_ausentes()
+
 from utils.pesca import main
 
 
